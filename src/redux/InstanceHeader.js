9a133b46733084067157d63b28e5364c69d3bd8c
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2023-09-01 23:11:15 +0700

    test

--- src/redux/InstanceHeader.js
+++ src/redux/InstanceHeader.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { BASE_URL } from "./Constants";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: BASE_URL
 })
 
 instance.interceptors.request.use(
