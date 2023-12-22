95487fabec4fb152e5457df6fc59936a66493fe4
sovita <sovita>
2023-12-22 15:54:00 +0700

    https api

--- src/redux/Constants.js
+++ src/redux/Constants.js
@@ -1,23 +1,23 @@
-import axios from "axios"
-import { toast } from "react-hot-toast"
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
-export const BASE_URL = `https://klassysignature.com/`
-// export const BASE_URL = `http://localhost:8080`
+// export const BASE_URL = `https://klassysignature.com/`
+export const BASE_URL = `https://sovita.yasokhen.info`;
 export const token = localStorage.getItem("token");
 
 export const API_HEADER = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Authorization': `Bearer ${token}`,
-    },
-})
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
 export const API = axios.create({
-    baseURL: BASE_URL,
-})
+  baseURL: BASE_URL,
+});
 
 export const notifySuccess = (e) => {
-    toast.success(e);
-}
+  toast.success(e);
+};
 export const notifyError = (e) => {
-    toast.error(e);
-}
+  toast.error(e);
+};
--- src/redux/InstanceHeader.js
+++ src/redux/InstanceHeader.js
@@ -2,19 +2,19 @@ import axios from "axios";
 import { BASE_URL } from "./Constants";
 
 export const instance = axios.create({
-    baseURL: BASE_URL
-})
+  baseURL: BASE_URL,
+});
 
 instance.interceptors.request.use(
-    config => {
-        var token = localStorage.getItem("token");
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`
-            config.headers['Content-Type'] = 'application/json';
-        }
-        return config
-    },
-    error => {
-        Promise.reject(error)
+  (config) => {
+    var token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers["Content-Type"] = "application/json";
     }
-)
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
